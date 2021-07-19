40f309d8828bff04ed595ea73e15152c9bf299d0
helge <helge@cerum.no>
2021-07-19 18:08:40 +0200

    Removed unused styling

--- src/components/ContactFormContent.svelte
+++ src/components/ContactFormContent.svelte
@@ -45,11 +45,6 @@
 </div>
 
 <style>
-  .contact-bg {
-    background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
-    background-size: 400% 400%;
-    animation: change 20s ease-in-out infinite;
-  }
   .glass-effect {
     background: linear-gradient(
       to right bottom,
