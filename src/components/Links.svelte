06a4c1d618eaf5ec04599c870086e6c8f4a008d1
helgelol <helge@kubes.no>
2022-03-05 21:54:02 +0100

    Typescript and link fixing

--- src/components/Links.svelte
+++ src/components/Links.svelte
@@ -1,26 +1,25 @@
-<script>
+<script lang="ts">
   import { svg_element } from 'svelte/internal';
 </script>
 
 <div class="my-8 mx-auto">
-  <a href="https://gitlab.com/helgejs" target="_blank">
-    <i class="fab fa-gitlab text-4xl mx-6 gitlab" />
+  <a href="https://github.com/helgelol/" target="_blank">
+    <i class="fab fa-github text-4xl mx-6 github" />
   </a>
-  <a href="https://twitter.com/helgejs" target="_blank">
+  <a href="https://twitter.com/helgelol" target="_blank">
     <i class="fab fa-twitter text-4xl mx-6 twitter" /></a
   >
-  <a href="https://www.linkedin.com/in/helgejs/" target="_blank">
+  <a href="https://www.linkedin.com/in/helgelol/" target="_blank">
     <i class="fab fa-linkedin-in text-4xl mx-6 linkedin" />
   </a>
 </div>
 
 <style>
-  .gitlab {
+  .github {
     color: white;
   }
-  .gitlab:hover {
-    /* color: linear-gradient(90deg, #e24329 0%, #fc6d26 50%, #fca326 100%); */
-    color: #fc6d26;
+  .github:hover {
+    color: #171515;
   }
   .twitter {
     color: white;
