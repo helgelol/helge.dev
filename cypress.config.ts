2058986a9a58ca9573ed26b05e080e3e0995d739
helgelol <helge@kubes.no>
2022-08-15 11:44:02 +0200

    Setting Cypress supportFile to false

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    supportFile: false,
     setupNodeEvents(on, config) {},
     baseUrl: 'http://localhost:3000',
   },
-})
+});
