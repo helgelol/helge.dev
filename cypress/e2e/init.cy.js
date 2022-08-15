bf7640342dbed9ef700edd14afce0d475e468d0a
helgelol <helge@kubes.no>
2022-08-15 13:50:52 +0200

    Added more tests

--- cypress/e2e/init.cy.js
+++ cypress/e2e/init.cy.js
@@ -1,9 +1,25 @@
 describe('Cypress', () => {
-  it('is working', () => {
-    expect(true).to.equal(true);
+  beforeEach(() => {
+    cy.visit('/');
   });
 
-  it('visits the app', () => {
-    cy.visit('/');
+  it('Page should have a title', () => {
+    cy.title().should('include', 'KUBES');
+  });
+
+  it('Should use UTF-8', () => {
+    cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
+  });
+
+  it('Should contain Name', () => {
+    cy.contains('h2', 'Helge Falch');
+  });
+
+  it('Name should have a title', () => {
+    cy.contains('p', 'System Developer');
+  });
+
+  it('Page should have hrefs', () => {
+    cy.get('a').should('have.attr', 'href', 'https://github.com/helgelol/');
   });
 });
