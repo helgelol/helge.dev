20bc3f4a2d8a40ec694a623111c305d0e9af6185
helgelol <helge@kubes.no>
2022-08-15 14:08:46 +0200

    Testing contact form rendering

--- cypress/e2e/init.cy.js
+++ cypress/e2e/init.cy.js
@@ -12,14 +12,20 @@ describe('Cypress', () => {
   });
 
   it('Should contain Name', () => {
-    cy.contains('h2', 'Helge Falch');
+    cy.contains('h2', 'Helge Falch').should('be.visible');
   });
 
   it('Name should have a title', () => {
-    cy.contains('p', 'System Developer');
+    cy.contains('p', 'System Developer').should('be.visible');
   });
 
   it('Page should have hrefs', () => {
     cy.get('a').should('have.attr', 'href', 'https://github.com/helgelol/');
+  });
+
+  it('Contact form renders', () => {
+    cy.contains('button', 'Contact').click().should('be.visible');
+    cy.get('form').should('be.visible');
+    cy.contains('button', 'Send').should('be.visible');
   });
 });
