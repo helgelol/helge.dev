2abbb31c0c7ccb1ba022a6530ea0f3ea78b90d13
helgelol <helge@kubes.no>
2023-03-21 14:10:34 +0100

    Removed cypress and set up playwright

---
+++ tests/kubes.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect, chromium } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:4173/');
+});
+
+test('Should have correct title', async ({ page }) => {
+  await expect(page).toHaveTitle('KUBES');
+});
+
+test('Should use UTF-8', async ({ page }) => {
+  const metaDescription = page.locator('meta[charset="UTF-8"]');
+  await expect(metaDescription).toHaveAttribute('charset', 'UTF-8');
+});
+
+test('Should contain name', async ({ page }) => {
+  await page.getByRole('heading', { name: 'Helge Falch' });
+});
+
+test('Should contain title', async ({ page }) => {
+  await page.getByText('System Developer');
+});
+
+test('Contact form works', async ({ page }) => {
+  await page.getByRole('button', { name: 'Contact' }).click();
+  await page.getByRole('button', { name: 'Send' }).click();
+});
