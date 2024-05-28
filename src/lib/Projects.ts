a54363dfc2987dbc39a94b50db6555cba134dcba
Helge Falch <helge@kubes.no>
2024-05-28 14:45:29 +0200

    Updated description of projects

--- src/lib/Projects.ts
+++ src/lib/Projects.ts
@@ -1,15 +1,17 @@
 const projects = [
 	{
-		title: 'Sniffler',
-		technologies: ['Go', 'Svelte'],
-		description: 'Personalized search engine',
-		url: 'https://gitlab.com/bzzt-no/sniffler'
+		title: 'Coffee',
+		technologies: ['Go', 'Svelte', 'MariaDb', 'Expo'],
+		description:
+			'Untappd-esque website and mobile application but for coffee beans. Backend in Go, Website in Svelte, and possibly an app in Expo.',
+		url: 'https://github.com/helgelol/coffee'
 	},
 	{
-		title: 'Coffee',
-		technologies: ['Go', 'Svelte', 'MariaDb'],
-		description: 'Untappd inspired coffee app',
-		url: 'https://github.com/helgelol/coffee'
+		title: 'Sniffler',
+		technologies: ['Go', 'Svelte'],
+		description:
+			'AI powered personalized search engine. Pardon the vague description, but the details are still being ironed out.',
+		url: 'https://gitlab.com/bzzt-no/sniffler'
 	}
 ];
 
