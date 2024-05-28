6c81ab55ab8ef876f0e13c0370ab5d13d8461273
Helge Falch <helge@kubes.no>
2024-05-28 22:38:47 +0200

    Updated projects

--- src/lib/Projects.ts
+++ src/lib/Projects.ts
@@ -1,17 +1,30 @@
 const projects = [
 	{
 		title: 'Coffee',
-		technologies: ['Go', 'Svelte', 'MariaDb', 'Expo'],
+		technologies: ['Go', 'Svelte', 'MariaDb', 'Expo', 'Docker'],
 		description:
 			'Untappd-esque website and mobile application but for coffee beans. Backend in Go, Website in Svelte, and possibly an app in Expo.',
 		url: 'https://github.com/helgelol/coffee'
 	},
 	{
 		title: 'Sniffler',
-		technologies: ['Go', 'Svelte'],
+		technologies: ['Go', 'Svelte', 'Docker'],
 		description:
 			'AI powered personalized search engine. Pardon the vague description, but the details are still being ironed out.',
 		url: 'https://gitlab.com/bzzt-no/sniffler'
+	},
+	{
+		title: 'Hmskort.no',
+		technologies: ['Angular', 'gRPC', 'C#', 'Docker', 'Kubernetes'],
+		description:
+			'Official website for ordering HSE cards from the Norwegian Labor Inspection Authority.',
+		url: 'https://hmskort.no'
+	},
+	{
+		title: 'Cerum.no',
+		technologies: ['Vue', 'PHP', 'Mysql', 'Docker'],
+		description: 'CRM/ERP hybrid product for Construction, Gyms, Cleaning and Welding.',
+		url: 'https://cerum.no'
 	}
 ];
 
