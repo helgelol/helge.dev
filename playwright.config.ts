f4e75072574a74ef9a08fcda11b0f163ec6d4e49
Helge Falch <helge@kubes.no>
2025-10-25 02:22:36 +0200

    Skipping webkit tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,10 +44,10 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
   ],
 
   /* Run your local dev server before starting the tests */
